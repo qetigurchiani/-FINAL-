753a8ed18d4f93278eb356ebe277bddfae4049e6
qetigurchiani <114924207+qetigurchiani@users.noreply.github.com>
2022-12-03 19:12:04 +0400

    integrated custom slider

--- script.js
+++ script.js
@@ -29,26 +29,39 @@ window.onscroll = function() {
 };
 
 //section3 -> slider
-const swiper = new Swiper('.swiper', {
-	// Optional parameters
-  
-	// If we need pagination
-	pagination: {
-	  el: '.swiper-pagination',
-	},
-  
-	// Navigation arrows
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
-  
-	// And if we need scrollbar
-	scrollbar: {
-	  el: '.swiper-scrollbar',
-	},
-  });
+let backward_sliderBtn = document.getElementById('backward-slider');
+let forward_sliderBtn = document.getElementById('forward-slider');
+	let product = document.getElementsByClassName('product')
+	let product_page = Math.ceil(product.length/4);
+	let l = 0;
+	let movePer = 25.34;
+	let maxMove = 203;
+	// mobile_view	
+	let mob_view = window.matchMedia("(max-width: 768px)");
+	if (mob_view.matches)
+	 {
+	 	movePer = 50.36;
+	 	maxMove = 504;
+	 }
 
+	let right_mover = ()=>{
+		l = l + movePer;
+		if (product == 1){l = 0; }
+		for(const i of product)
+		{
+			if (l > maxMove){l = l - movePer;}
+			i.style.left = '-' + l + '%';
+		}
 
-
-
+	}
+	let left_mover = ()=>{
+		l = l - movePer;
+		if (l<=0){l = 0;}
+		for(const i of product){
+			if (product_page>1){
+				i.style.left = '-' + l + '%';
+			}
+		}
+	}
+	forward_sliderBtn.onclick = ()=>{right_mover();}
+	backward_sliderBtn.onclick = ()=>{left_mover();}
