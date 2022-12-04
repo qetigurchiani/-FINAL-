cf1fe931d0fb8e400f33e7d74476516f2dd46a78
qetigurchiani <114924207+qetigurchiani@users.noreply.github.com>
2022-12-05 01:22:14 +0400

    added responsive

--- script.js
+++ script.js
@@ -34,13 +34,13 @@ let forward_sliderBtn = document.getElementById('forward-slider');
 	let product = document.getElementsByClassName('product')
 	let product_page = Math.ceil(product.length/4);
 	let l = 0;
-	let movePer = 25.34;
+	let movePer = 75.34;
 	let maxMove = 203;
 	// mobile_view	
 	let mob_view = window.matchMedia("(max-width: 768px)");
 	if (mob_view.matches)
 	 {
-	 	movePer = 50.36;
+	 	movePer = 100.36;
 	 	maxMove = 504;
 	 }
 
@@ -65,3 +65,79 @@ let forward_sliderBtn = document.getElementById('forward-slider');
 	}
 	forward_sliderBtn.onclick = ()=>{right_mover();}
 	backward_sliderBtn.onclick = ()=>{left_mover();}
+
+
+	//form validation
+let registrationForm = document.getElementById("resgitrationForm");
+
+registrationForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let errors = {};
+
+  //username
+  let usernameValue = document.getElementById("usernameField").value;
+  if (usernameValue == "" && usernameValue.length < 5) {
+    errors.username =
+      "Username field can not be empty and must include at least one capital letter";
+  }
+
+  //password
+  let passwordValue = document.getElementById("passwordField").value;
+  let passwordValue2 = document.getElementById("passwordFieldrepeat").value;
+
+  if (passwordValue == "") {
+    errors.password = "Password field can not be empty";
+  }
+  if (passwordValue != passwordValue2) {
+    errors.password2 = "Passwords do NOT macth";
+  }
+
+  //checkbox
+  let agreeField = document.getElementById("agreeTerms").checked;
+
+  if (!agreeField) {
+    errors.agree = "to continue you should agree our terms and conditions";
+  }
+
+  console.log(errors);
+
+  document.querySelectorAll(".error-text").forEach((item) => {
+    item.innerText = " ";
+  });
+
+  for (let key in errors) {
+    let spanText = document.getElementById("error_" + key);
+
+    if (spanText) {
+      spanText.innerText = errors[key];
+    }
+  }
+
+  if (Object.keys(errors).length == 0) {
+    registrationForm.submit();
+  }
+});
+
+
+// email regex validation
+let emailField = document.getElementById("emailField");
+
+emailField.addEventListener("keyup", function () {
+  let emailValue = document.getElementById("emailField").value;
+  let errorSpan = document.getElementById("text");
+
+  let emailPattern =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (emailValue.match(emailPattern)) {
+    errorSpan.innerText = "Your emal is Valid";
+    errorSpan.style.color = "green";
+  } else {
+    errorSpan.innerText = "Your emal is invalid";
+    errorSpan.style.color = "red";
+  }
+
+  if (emailValue == "") {
+    errorSpan.innerHTML = " ";
+  }
+});
