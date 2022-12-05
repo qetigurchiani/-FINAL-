a3b3c52fcf3e8eb592e9596606e54fe6d8363b69
qetigurchiani <114924207+qetigurchiani@users.noreply.github.com>
2022-12-05 17:53:05 +0400

    added responsive

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-"use strict";
-// navbar
-const mainmenu = document.querySelector('.mainmenu');
-const closemenu = document.querySelector('.closemenu');
-const openmenu = document.querySelector('.openmenu');
-
-openmenu.addEventListener('click',show);
-closemenu.addEventListener('click', close);
-
-function show(){
-    mainmenu.style.display = 'flex';
-    mainmenu.style.top = '0';
-}
-
-function close(){
-    mainmenu.style.top = '-100%';
-}
-
 // navbar + scroll
 
 let myNav = document.getElementById("nav");
